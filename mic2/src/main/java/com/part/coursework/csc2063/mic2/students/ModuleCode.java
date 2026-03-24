d1bd7b4f7b29038b7d79f60457927dd00f8358b0
brennanrobert05 <brennanrobert05@gmail.com>
2026-03-24 15:24:03 +0000

    Avoiding Merge Conflict

--- mic2/src/main/java/com/part/coursework/csc2063/mic2/students/ModuleCode.java
+++ mic2/src/main/java/com/part/coursework/csc2063/mic2/students/ModuleCode.java
@@ -1,11 +1,5 @@
 package com.part.coursework.csc2063.mic2.students;
 
-public enum ModuleCode {
-    CSC2063,
-    CSC2001,
-    CSC2002,
-    CSC3001,
-    CSC3002,
-    CSC4001,
-    CSC4002
+public class ModuleCode {
+
 }
--- mic2/src/main/java/com/part/coursework/csc2063/mic2/students/Students.java
+++ mic2/src/main/java/com/part/coursework/csc2063/mic2/students/Students.java
@@ -1,30 +1,5 @@
 package com.part.coursework.csc2063.mic2.students;
-import java.util.ArrayList;
 
 public class Students {
 
-    private ArrayList<Student> students = new ArrayList<>();
-
-    public Students() {}
-
-    public ArrayList<Student> getStudents() {
-        return students;
-    }
-
-    public void setStudents(ArrayList<Student> students) {
-        this.students = (students == null) ? new ArrayList<>() : students;
-    }
-
-    public void addStudent(Student student) {
-        if (student == null) return;
-        this.students.add(student);
-    }
-
-    public Student findStudentById(String id) {
-        if (id == null) return null;
-        for (Student s : students) {
-            if (id.equals(s.getId())) return s;
-        }
-        return null;
-    }
 }
